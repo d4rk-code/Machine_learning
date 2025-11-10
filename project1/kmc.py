9cf9453766191387682286dea13e5a218ff907c3
d4rk-code <jalajsrivastavadpsjkp.3823@gmail.com>
2025-11-10 16:05:49 +0530

    kmc update

--- project1/kmc.py
+++ project1/kmc.py
@@ -2,34 +2,56 @@
 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import random
 
-#reading data
+'''reading data'''
 
 data = pd.read_csv("kmeans_dataset.csv")
 x = data["X"].to_list()
 y = data["Y"].to_list()
 
-#implementing the algorithm
+'''implementing the algorithm'''
 
 # step 1 : select the k centroids , step 2: categorize the given points according to the k centroids
 # step 3 : recalculate the position of the centroids
 
-k = 3
+k = 3 # because the dataset that i chose has 3 clusters provided
+
 
 # random selection of centroids
-c = [[2.08,8.01],[7.05,7.02],[1.88,1.88]]
-point = [2.67,6.52]
+#c = [[2.08,8.01],[7.05,7.02],[1.88,1.88]]  desired centroids
+
+c = []
+
+for j in range(3):
+    c.append([random.uniform(0.00,9.50),random.uniform(0.00,9.00)])
 
 
 def euclidian_distance(x , x_):
-    dist_x = (x[0] - x_[0])**2 
-    dist_y = (x[1] - x_[1])**2 
-    d = (dist_x + dist_y)**0.5
-    return d
+    dist = ((x[0] - x_[0])**2 + (x[1] - x_[1])**2)**0.5
+    return dist
 
-dist = euclidian_distance(c[0], point)
-print(dist)
+#find the minimum distances between the centroids and the points in the dataset
 
-#plot of the data
+l = []
+
+for i in range(len(x)):
+    #print([x[i],y[i]])
+    q = []
+    for j in range(k):
+        d = euclidian_distance([x[i],y[i]], c[j])
+        q.append(d)
+    l.append(q)
+
+# the nearest distance
+l.sort()
+print(l[:1])
+
+# classification 
+
+
+# recalculation of the centroids
+
+'''plot of the data'''
 #plt.scatter(x,y)
 #plt.show()
